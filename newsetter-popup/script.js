07c91419ad6e595d9e74d7c15628ee32a7175d2b
Arjun Sharma <arjun131315.com>
2026-01-22 02:00:47 +0530

    add logic to overlay

--- newsetter-popup/script.js
+++ newsetter-popup/script.js
@@ -1,9 +1,14 @@
 const button=document.querySelector("button")
-const popUp=document.querySelector(".pop-up")
+const popUp=document.querySelector(".popup-container")
 const closeIcon=document.querySelector('.close-icon')
+const overlay=document.querySelector('.overlay')
+
 button.addEventListener('click',()=>{
-    popUp.classList.add('open')
+    popUp.classList.add('popup-open')
 })
 closeIcon.addEventListener('click',()=>{
-    popUp.classList.remove('open')
+    popUp.classList.remove('popup-open')
+})
+overlay.addEventListener('click',()=>{
+    popUp.classList.remove('popup-open')
 })
